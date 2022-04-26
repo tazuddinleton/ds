dec98796f95c44bc7d63091b7c9ce461445b6c93
Taz Uddin <tazuddinleton@gmail.com>
2022-04-26 21:50:15 +0600

    Add doc comments

--- singly/singly.go
+++ singly/singly.go
@@ -2,15 +2,18 @@ package singly
 
 import "fmt"
 
+// Node describes a singly linked list data structure
 type Node[T any] struct {
 	data T
 	next *Node[T]
 }
 
+// New creates a new singly linked list
 func New[T any](data T, next *Node[T]) *Node[T] {
 	return &Node[T]{data, next}
 }
 
+// NewList a variadic constructor for singly linked list
 func NewList[T any](ts ...T) *Node[T] {
 	if len(ts) <= 0 {
 		return nil
@@ -18,6 +21,7 @@ func NewList[T any](ts ...T) *Node[T] {
 	return New(ts[0], NewList((ts[1:])...))
 }
 
+// PrintList prints the data to console sequentially
 func PrintList[T any](node *Node[T]) {
 	if node == nil {
 		return
@@ -26,6 +30,7 @@ func PrintList[T any](node *Node[T]) {
 	PrintList(node.next)
 }
 
+// Returns a flat slice representation of the singly linked list
 func Flatten[T any](node *Node[T]) []T {
 	var loop func(*Node[T], []T) []T
 	loop = func(n *Node[T], t []T) []T {
@@ -37,6 +42,7 @@ func Flatten[T any](node *Node[T]) []T {
 	return loop(node, []T{})
 }
 
+// FlattenSelect works similart to Flatten but lets user pass a selector to what data should be selected
 func FlattenSelect[T any, P any](node *Node[T], f func(T) P) []P {
 	var loop func(*Node[T], []P) []P
 	loop = func(n *Node[T], t []P) []P {
