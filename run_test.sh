0f95737099186540fa903fee7deab0afae17e5f6
Taz Uddin <getleton@gmail.com>
2022-09-04 23:27:49 +0600

    Update run_test.sh to run all tests by default with verbose mode

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,37 @@
+#!/bin/sh
 
-go test -v -count=1 -run  $2 $1
+# By default run all tests with -verbose mode on and clean all cache with count = 1 
+if [ $# -eq 0 ]
+then
+  go test -v -count=1 ./...
+  exit 0
+fi
+
+
+while getopts t:p:h: flag 
+do 
+  case "${flag}" in 
+    t) test_name="${OPTARG}";;
+    p) package_path="${OPTARG}";;
+    h) help=true;;
+    *) help=true;;
+  esac
+done
+
+if [ $help ]
+then
+  printf "%s accepts follwoing args: 
+    -t name of the test 
+    -p path to the package
+    
+    If you want to run all tests invoke without any args.
+
+    \n" "${0}"
+  exit 0
+fi
+
+if [ "$test_name" ] && [ "$package_path" ]
+then
+  go test -v -count=1 -run  "$test_name" "$package_path"
+fi
+
